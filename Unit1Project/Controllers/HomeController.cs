c6755c73d384f3fed72a67ee7e3d61e947aa57f1
Firaol Baneta <firaolbaneta99@gmail.com>
2023-09-18 13:03:59 -0500

    Add project files.

---
+++ Unit1Project/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Unit1Project.Models;
+
+namespace Unit1Project.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult CalculateAge(Birthdate model)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.FV = model.CalculateAge();
+            }
+            else
+            {
+                ViewBag.FV = 0;
+            }
+            return View(model);
+        }
+
+
+
+    }
+}
---
+++ Unit1Project/Models/Birthdate.cs
@@ -0,0 +1,25 @@
+﻿namespace Unit1Project.Models
+{/// <summary>
+/// 
+/// </summary>
+    public class Birthdate
+    {
+        public DateTime BirthDate { get; set; }
+        public int Age => CalculateAge();
+
+
+        public int CalculateAge()
+        {
+            var today = DateTime.Today;
+            var age = today.Year - BirthDate.Year;
+
+
+            // If statement
+            if (BirthDate.Date > today.AddYears(-age)) age--;
+            return age;
+
+
+
+        }
+    }
+}
